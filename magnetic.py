8c99522e8ef2a97c594375e6da6e4894a78d7cd5
ssskkkky <chenguangsha@foxmail.com>
2020-08-16 22:51:15 +0800

    add coil surface current

--- magnetic.py
+++ magnetic.py
@@ -7,8 +7,12 @@ def MakeGeometry():
     box = OrthoBrick(Pnt(-1,-1,-1),Pnt(2,1,2)).bc("outer")
 
     core = OrthoBrick(Pnt(0.45,-0.05,0),Pnt(0.55,0.05,1)).maxh(0.2).mat("core")
+    coil = (Cylinder(Pnt(0.05,0,0), Pnt(0.05,0,1), 0.3) - \
+        Cylinder(Pnt(0.05,0,0), Pnt(0.05,0,1), 0.15))# * \
+        # OrthoBrick (Pnt(-1,-1,0.3),Pnt(1,1,0.7)).maxh(0.2).mat("coil")
     geometry.Add ((box-core).mat("air"))
     geometry.Add (core)
+    geometry.Add(coil)
     return geometry
 
 ngmesh = MakeGeometry().GenerateMesh(maxh=0.5)
